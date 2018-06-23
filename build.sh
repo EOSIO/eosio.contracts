063ddf714cd6aa79bf516c40438e79690f5715c2
Bucky Kittinger <larrykittinger@gmail.com>
2018-06-23 18:51:40 -0400

    Fixed build.sh and started on unit test support, a lot of undefined references

--- build.sh
+++ build.sh
@@ -6,16 +6,17 @@ unamestr=`uname`
 if [[ "${unamestr}" == 'Darwin' ]]; then
    PREFIX=/usr/local
 else
-   PREFIX=/opt
+   PREFIX=~/opt
+   BOOST=~/opt/boost/include
 fi
 
 mkdir -p bin/${CONTRACT_NAME}
 ### BUILD THE CONTRACT
-EOSCLANG="${PREFIX}/wasm/bin/clang++ -I/usr/local/include/libc++/upstream/include -I/usr/local/include/musl/upstream/include -I/usr/local/include -Ideps/eosio.token/include -Ideps/eosio.exchange/include "
+EOSCLANG="${PREFIX}/wasm/bin/clang++ -I/usr/local/include/libc++/upstream/include -I/usr/local/include/musl/upstream/include -I/usr/local/include -Ideps/eosio.token/include -Ideps/eosio.exchange/include -I${BOOST}"
 LINK="${PREFIX}/wasm/bin/llvm-link -only-needed "
 LLC="${PREFIX}/wasm/bin/llc -thread-model=single --asm-verbose=false"
-S2W="${PREFIX}/bin/eosio-s2wasm "
-W2W="${PREFIX}/bin/eosio-wast2wasm "
+S2W="/usr/local/bin/eosio-s2wasm "
+W2W="/usr/local/bin/eosio-wast2wasm "
 ${EOSCLANG}  -Iinclude -c -emit-llvm -O3 --std=c++14 --target=wasm32 -nostdinc -DBOOST_DISABLE_ASSERTS -DBOOST_EXCEPTION_DISABLE -nostdlib -nostdlibinc -ffreestanding -nostdlib -fno-threadsafe-statics -fno-rtti -fno-exceptions -o ${CONTRACT_NAME}.bc src/${CONTRACT_NAME}.cpp
 ${LINK} -o linked.bc ${CONTRACT_NAME}.bc /usr/local/usr/share/eosio/contractsdk/lib/eosiolib.bc /usr/local/usr/share/eosio/contractsdk/lib/libc++.bc /usr/local/usr/share/eosio/contractsdk/lib/libc.bc
 ${LLC} -o ${CONTRACT_NAME}.s linked.bc
