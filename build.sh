358366ec4116e3164b98705a9af51b498d5d7134
Bucky Kittinger <larrykittinger@gmail.com>
2018-07-27 03:09:11 -0400

    Cleaning up, and fixes for pointing to proper sdk and eosio

--- build.sh
+++ build.sh
@@ -5,19 +5,9 @@ printf "\t=========== Building eosio.contracts ===========\n\n"
 RED='\033[0;31m'
 NC='\033[0m'
 
-#if [ ! -d "/usr/local/eosio" ]; then
-#   printf "${RED}Error, please ensure that eosio is installed correctly!\n\n${NC}"
-#   exit -1
-#fi
-
-if [ ! -d "/usr/local/eosio.wasmsdk" ]; then
-   printf "${RED}Error, please ensure that eosio.wasmsdk is installed correctly!\n\n${NC}"
-   exit -1
-fi
-
 CORES=`getconf _NPROCESSORS_ONLN`
 mkdir -p build
 pushd build &> /dev/null
-cmake -DEOSIO_ROOT=/Users/judgefudge/master_eos/eos/build ../
+cmake ../
 make -j${CORES}
 popd &> /dev/null
