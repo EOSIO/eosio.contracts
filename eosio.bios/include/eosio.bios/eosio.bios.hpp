a13054f0b57b938d1d36d7d74663d30a6dcb113a
Bucky Kittinger <larrykittinger@gmail.com>
2018-11-26 17:00:48 -0500

    pull all of native.hpp in

--- eosio.bios/include/eosio.bios/eosio.bios.hpp
+++ eosio.bios/include/eosio.bios/eosio.bios.hpp
@@ -1,39 +1,79 @@
 #pragma once
+#include <eosiolib/action.hpp>
 #include <eosiolib/crypto.h>
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/privileged.hpp>
-#include <eosiolib/action.hpp>
-#include <eosio.system/native.hpp>
+#include <eosiolib/producer_schedule.hpp>
 
 namespace eosio {
+   using eosio::permission_level;
+   using eosio::public_key;
+   using eosio::ignore;
+
+   struct permission_level_weight {
+      permission_level  permission;
+      uint16_t          weight;
+
+      // explicit serialization macro is not necessary, used here only to improve compilation time
+      EOSLIB_SERIALIZE( permission_level_weight, (permission)(weight) )
+   };
+
+   struct key_weight {
+      eosio::public_key  key;
+      uint16_t           weight;
+
+      // explicit serialization macro is not necessary, used here only to improve compilation time
+      EOSLIB_SERIALIZE( key_weight, (key)(weight) )
+   };
+
+   struct wait_weight {
+      uint32_t           wait_sec;
+      uint16_t           weight;
+
+      // explicit serialization macro is not necessary, used here only to improve compilation time
+      EOSLIB_SERIALIZE( wait_weight, (wait_sec)(weight) )
+   };
+
+   struct authority {
+      uint32_t                              threshold = 0;
+      std::vector<key_weight>               keys;
+      std::vector<permission_level_weight>  accounts;
+      std::vector<wait_weight>              waits;
+
+      // explicit serialization macro is not necessary, used here only to improve compilation time
+      EOSLIB_SERIALIZE( authority, (threshold)(keys)(accounts)(waits) )
+   };
+
+   struct block_header {
+      uint32_t                                  timestamp;
+      name                                      producer;
+      uint16_t                                  confirmed = 0;
+      capi_checksum256                          previous;
+      capi_checksum256                          transaction_mroot;
+      capi_checksum256                          action_mroot;
+      uint32_t                                  schedule_version = 0;
+      std::optional<eosio::producer_schedule>   new_producers;
+
+      // explicit serialization macro is not necessary, used here only to improve compilation time
+      EOSLIB_SERIALIZE(block_header, (timestamp)(producer)(confirmed)(previous)(transaction_mroot)(action_mroot)
+                                     (schedule_version)(new_producers))
+   };
 
    class [[eosio::contract("eosio.bios")]] bios : public contract {
       public:
          using contract::contract;
-         /**
-          *  Called after a new account is created. This code enforces resource-limits rules
-          *  for new accounts as well as new account naming conventions.
-          *
-          *  1. accounts cannot contain '.' symbols which forces all acccounts to be 12
-          *  characters long without '.' until a future account auction process is implemented
-          *  which prevents name squatting.
-          *
-          *  2. new accounts must stake a minimal number of tokens (as set in system parameters)
-          *     therefore, this method will execute an inline buyram from receiver for newacnt in
-          *     an amount equal to the current new account creation fee.
-          */
          [[eosio::action]]
          void newaccount( name             creator,
                           name             name,
-                          ignore<eosiosystem::authority> owner,
-                          ignore<eosiosystem::authority> active){}
+                          ignore<authority> owner,
+                          ignore<authority> active){}
 
 
          [[eosio::action]]
          void updateauth(  ignore<name>  account,
                            ignore<name>  permission,
                            ignore<name>  parent,
-                           ignore<eosiosystem::authority> auth ) {}
+                           ignore<authority> auth ) {}
 
          [[eosio::action]]
          void deleteauth( ignore<name>  account,
