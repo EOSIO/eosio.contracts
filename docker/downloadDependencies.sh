522e8c0bcd1fb1503c6708c81f53e7484b4405bb
Zach Butler <zach.butler@block.one>
2019-02-15 12:36:28 -0500

    Added some print statements, reordered some commands

--- docker/downloadDependencies.sh
+++ docker/downloadDependencies.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 CONTRACTS_PATH='/eosio.contracts' # location in Docker container, not your local machine
 # get EOSIO
-echo "downloadDependencies.sh - Downloading EOSIO source from GitHub..."
+echo "downloadDependencies.sh - Cloning EOSIO source from GitHub..."
 EOSIO_VERSION=$(cat $CONTRACTS_PATH/dependencies | sed 's,//,#,g' | cut -d '#' -f 1 | grep -v 'eos.*cdt.*=' | grep -v 'eos.*contracts.*=' | grep '.*eos.*=' | cut -d '=' -f 2 | awk '{print $1}')
 EOSIO_COMMIT=$((curl -s https://api.github.com/repos/EOSIO/eos/git/refs/tags/$EOSIO_VERSION && curl -s https://api.github.com/repos/EOSIO/eos/git/refs/heads/$EOSIO_VERSION) | jq '.object.sha' | grep -v 'null' | tr -d '"' | sed -n '1p') # search GitHub for commit hash by tag and branch, preferring tag if both match
 test -z $EOSIO_COMMIT && EOSIO_COMMIT=$(echo $EOSIO_VERSION | tr -d '"' | tr -d "''" | awk '{print $1}') # if both searches returned nothing, the version is probably specified by commit hash already
 git clone -n https://github.com/EOSIO/eos.git --recursive --quiet
+echo "downloadDependencies.sh - Cloned EOSIO. Checking out ${EOSIO_COMMIT:0:7}..."
 cd eos
 test "$(git cat-file -t $EOSIO_COMMIT 2>/dev/null)" != "commit" && cd .. && echo "downloadDependencies.sh - ERROR: EOSIO tag, branch, or commit \"$EOSIO_VERSION\" not found in github.com/EOSIO/eos$(test $EOSIO_VERSION != $EOSIO_COMMIT && echo " using commit \"${EOSIO_COMMIT:0:7}\"")! Exiting..." && exit 1
 git checkout $EOSIO_COMMIT --quiet
+echo "downloadDependencies.sh - Checked out EOSIO at $EOSIO_COMMIT from \"$EOSIO_VERSION\""'!'
 echo "$CONTRACTS_BRANCH:$EOSIO_COMMIT" > /etc/eosio-version # this is necessary for our eos CMake VersionMacros which construct the string returned from "$ nodeos --version"; CONTRACTS_BRANCH is a build-arg passed from pipeline.yml into the Docker container
+echo "downloadDependencies.sh - Wrote \"/etc/eosio-version\"."
 cd ..
-echo "downloadDependencies.sh - Downloaded EOSIO at $EOSIO_COMMIT from \"$EOSIO_VERSION\""'!'
 # get CDT
 echo "downloadDependencies.sh - Getting CDT binary from Amazon S3..."
 CDT_VERSION=$(cat $CONTRACTS_PATH/dependencies | sed 's,//,#,g' | cut -d '#' -f 1 | grep 'cdt.*=' | cut -d '=' -f 2 | awk '{print $1}')
