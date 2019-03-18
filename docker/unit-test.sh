a5e1a5e62da3544ab1c83edafd2b70b7414fcacc
Zach Butler <zach.butler@block.one>
2019-03-18 11:58:01 -0400

    Added xUnit-formatted XML test result output; parallelization now scales across cores

--- docker/unit-test.sh
+++ docker/unit-test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e # exit on failure of any "simple" command (excludes &&, ||, or | chains)
+CPU_CORES=$(getconf _NPROCESSORS_ONLN)
+echo "$CPU_CORES cpu cores detected."
 cd /eosio.contracts/build/tests
-ctest -j8 --output-on-failure
+echo "$ ctest -j $CPU_CORES --output-on-failure -T Test"
+ctest -j $CPU_CORES --output-on-failure -T Test
+mv /eosio.contracts/build/tests/Testing/$(ls /eosio.contracts/build/tests/Testing/ | grep '20' | tail -n 1)/Test.xml /artifacts/Test.xml
