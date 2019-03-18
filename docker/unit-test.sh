d197b62af8aa8333dadf3bed5623fbb3f228a9a7
Zach Butler <zach.butler@block.one>
2019-03-18 14:52:45 -0400

    Added code to check that ctest has non-zero registered tests

--- docker/unit-test.sh
+++ docker/unit-test.sh
@@ -3,6 +3,8 @@ set -e # exit on failure of any "simple" command (excludes &&, ||, or | chains)
 CPU_CORES=$(getconf _NPROCESSORS_ONLN)
 echo "$CPU_CORES cpu cores detected."
 cd /eosio.contracts/build/tests
+TEST_COUNT=$(ctest -N | grep -i 'Total Tests: ' | cut -d ':' -f 2 | awk '{print $1}')
+[[ $TEST_COUNT > 0 ]] && echo "$TEST_COUNT tests found." || (echo "ERROR: No tests registered with ctest! Exiting..." && exit 1)
 echo "$ ctest -j $CPU_CORES --output-on-failure -T Test"
 ctest -j $CPU_CORES --output-on-failure -T Test
 mv /eosio.contracts/build/tests/Testing/$(ls /eosio.contracts/build/tests/Testing/ | grep '20' | tail -n 1)/Test.xml /artifacts/Test.xml
