765310c71682ee93a9c920cf4ebf04e6cda2d39f
Khaled Al-Hassanieh <khaled.alhassanieh@gmail.com>
2019-03-21 15:07:57 -0400

    Add additional check specific to b1 account

--- contracts/eosio.system/src/rex.cpp
+++ contracts/eosio.system/src/rex.cpp
@@ -141,6 +141,9 @@ namespace eosiosystem {
       auto current_order = fill_rex_order( bitr, rex );
       asset pending_sell_order = update_rex_account( from, current_order.proceeds, current_order.stake_change );
       if ( !current_order.success ) {
+         if ( from == "b1"_n ) {
+            check( false, "b1 sellrex orders should not be queued" );
+         }
          /**
           * REX order couldn't be filled and is added to queue.
           * If account already has an open order, requested rex is added to existing order.
