cad0c500785abdefd0b206c3dfa6ce62381af0b5
Khaled Al-Hassanieh <khaled.alhassanieh@gmail.com>
2019-04-19 16:42:15 -0400

    Changes following @arhag code review

--- contracts/eosio.system/include/eosio.system/exchange_state.hpp
+++ contracts/eosio.system/include/eosio.system/exchange_state.hpp
@@ -32,10 +32,20 @@ namespace eosiosystem {
 
       uint64_t primary_key()const { return supply.symbol.raw(); }
 
-      asset convert_to_exchange( connector& c, const asset& in );
-      asset convert_from_exchange( connector& c, const asset& in );
+      asset convert_to_exchange( connector& reserve, const asset& payment );
+      asset convert_from_exchange( connector& reserve, const asset& tokens );
       asset convert( const asset& from, const symbol& to );
       asset direct_convert( const asset& from, const symbol& to );
+      /**
+       * Given two connector balances (inp_reserve and out_reserve), and an incoming amount
+       * of inp, this function calculates the delta out using Banacor equation.
+       *
+       * @param inp - input amount, same units as inp_reserve
+       * @param inp_reserve - the input connector balance
+       * @param out_reserve - the output connector balance
+       *
+       * @return int64_t - conversion output amount
+       */
       static int64_t get_bancor_output( int64_t inp_reserve,
                                         int64_t out_reserve,
                                         int64_t inp );
