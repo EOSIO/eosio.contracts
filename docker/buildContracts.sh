46674ceaff41c08d7baae97e7ea58b324ca6d929
Scott Arnette <scott.a.arnette@gmail.com>
2019-06-20 13:43:41 -0400

    Added -y flag to ensure non-interactive mode.

--- docker/buildContracts.sh
+++ docker/buildContracts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e # exit on failure of any "simple" command (excludes &&, ||, or | chains)
 cd /eosio.contracts
-./build.sh -c /usr/opt/eosio.cdt -e /opt/eosio
+./build.sh -c /usr/opt/eosio.cdt -e /opt/eosio -y
 cd build
 tar -pczf /artifacts/contracts.tar.gz *
