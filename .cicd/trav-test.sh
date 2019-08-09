6fdf63868855f9035fc1cbe17f8745f097a0989d
Scott Arnette <scott.a.arnette@gmail.com>
2019-08-08 20:17:00 -0400

    Fixed test location.

--- .cicd/trav-test.sh
+++ .cicd/trav-test.sh
@@ -9,7 +9,7 @@ echo '$ cmake ..'
 cmake ..
 echo "$ make -j $CPU_CORES"
 make -j $CPU_CORES
-cd /eosio.contracts/build/tests
+cd /workdir/build/tests
 TEST_COUNT=$(ctest -N | grep -i 'Total Tests: ' | cut -d ':' -f 2 | awk '{print $1}')
 [[ $TEST_COUNT > 0 ]] && echo "$TEST_COUNT tests found." || (echo "ERROR: No tests registered with ctest! Exiting..." && exit 1)
 echo "$ ctest -j $CPU_CORES"
