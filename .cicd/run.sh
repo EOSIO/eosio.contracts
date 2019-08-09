95442233b5bb1ca306870689a36821a76e0e43f2
Scott Arnette <scott.a.arnette@gmail.com>
2019-08-09 09:23:42 -0400

    Ensure that run.sh was broken for Travis.

--- .cicd/run.sh
+++ .cicd/run.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 set -eo pipefail
 . ./.cicd/helpers/general.sh
-. ./$HELPERS_DIR/execute.sh
+. ./.cicd/helpers/execute.sh
 
 export FULL_TAG="eosio/producer:eos-binaries-trav-poc-contract-tests-1.8.0-e13ec7f756e78d9baf994c5d3a7bd643653d834b"
-# export FULL_TAG="eosio/producer:eos-binaries-trav-poc-contract-support-1.8.0"
 export CDT_VERSION="1.6.2"
 
 if [[ $(uname) == Darwin ]]; then
