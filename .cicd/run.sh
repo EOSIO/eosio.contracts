3db80202fcbf027398c4b2467cae4f756a6a67f2
Scott Arnette <scott.a.arnette@gmail.com>
2019-08-09 16:25:00 -0400

    Contracts won't need to build on other systems. Remove Darwin conditional.

--- .cicd/run.sh
+++ .cicd/run.sh
@@ -6,41 +6,25 @@ set -eo pipefail
 export FULL_TAG="eosio/ci-contracts-builder:base-ubuntu-18.04-latest"
 # export FULL_TAG="eosio/producer:eos-binaries-trav-poc-contract-tests-1.8.0-e13ec7f756e78d9baf994c5d3a7bd643653d834b"
 # export CDT_VERSION="1.6.2"
-
-if [[ $(uname) == Darwin ]]; then
-
-    cd $ROOT_DIR
-    ccache -s
-    mkdir -p build
-    cd build
-    execute cmake ..
-    execute make -j$JOBS
-    cd tests
-    execute ctest -j$JOBS -V --output-on-failure -T Test
-    cd ..
     
-else # Linux
+. ./$HELPERS_DIR/docker.sh
 
-    . ./$HELPERS_DIR/docker.sh
-    
-    # [[ -z $CDT_VERSION ]] && echo "Please specify CDT_VERSION." && exit 1
-    # CDT_COMMANDS="curl -LO https://github.com/EOSIO/eosio.cdt/releases/download/v$CDT_VERSION/eosio.cdt_$CDT_VERSION-1-ubuntu-18.04_amd64.deb && dpkg -i eosio.cdt_$CDT_VERSION-1-ubuntu-18.04_amd64.deb && export PATH=/usr/opt/eosio.cdt/$CDT_VERSION/bin:$PATH"
-    BUILD_COMMANDS="mkdir -p /workdir/build && cd /workdir/build && cmake -DCMAKE_CXX_COMPILER='clang++' -DCMAKE_C_COMPILER='clang' -DCMAKE_FRAMEWORK_PATH='/usr/local' .. && make -j $JOBS"
-    TEST_COMMANDS="cd /workdir/build/tests && ctest -j $JOBS -V --output-on-failure -T Test"
+# [[ -z $CDT_VERSION ]] && echo "Please specify CDT_VERSION." && exit 1
+# CDT_COMMANDS="curl -LO https://github.com/EOSIO/eosio.cdt/releases/download/v$CDT_VERSION/eosio.cdt_$CDT_VERSION-1-ubuntu-18.04_amd64.deb && dpkg -i eosio.cdt_$CDT_VERSION-1-ubuntu-18.04_amd64.deb && export PATH=/usr/opt/eosio.cdt/$CDT_VERSION/bin:$PATH"
+BUILD_COMMANDS="mkdir -p /workdir/build && cd /workdir/build && cmake -DCMAKE_CXX_COMPILER='clang++' -DCMAKE_C_COMPILER='clang' -DCMAKE_FRAMEWORK_PATH='/usr/local' .. && make -j $JOBS"
+TEST_COMMANDS="cd /workdir/build/tests && ctest -j $JOBS -V --output-on-failure -T Test"
 
-    # Docker Run Arguments
-    ARGS=${ARGS:-"--rm -v $(pwd):/workdir"}
-    # Docker Commands
-    if [[ $BUILDKITE ]]; then
-        # append-to-commands $CDT_COMMANDS
-        [[ $ENABLE_BUILD ]] && append-to-commands $BUILD_COMMANDS
-        [[ $ENABLE_TEST ]] && append-to-commands $TEST_COMMANDS
-    elif [[ $TRAVIS ]]; then
-        ARGS="$ARGS -e JOBS"
-        COMMANDS="ccache -s && $BUILD_COMMANDS && $TEST_COMMANDS"
-    fi
-
-    # Docker Run
-    docker-run $COMMANDS
-
+# Docker Run Arguments
+ARGS=${ARGS:-"--rm -v $(pwd):/workdir"}
+# Docker Commands
+if [[ $BUILDKITE ]]; then
+    # append-to-commands $CDT_COMMANDS
+    [[ $ENABLE_BUILD ]] && append-to-commands $BUILD_COMMANDS
+    [[ $ENABLE_TEST ]] && append-to-commands $TEST_COMMANDS
+elif [[ $TRAVIS ]]; then
+    ARGS="$ARGS -e JOBS"
+    COMMANDS="ccache -s && $BUILD_COMMANDS && $TEST_COMMANDS"
 fi
+
+# Docker Run
+docker-run $COMMANDS
