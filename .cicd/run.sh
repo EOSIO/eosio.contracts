47b2cf29d4edc1ec4c21848e40305b145535afb4
Scott Arnette <scott.a.arnette@gmail.com>
2019-08-09 09:47:29 -0400

    Hard specify path to docker.sh.

--- .cicd/run.sh
+++ .cicd/run.sh
@@ -18,7 +18,7 @@ if [[ $(uname) == Darwin ]]; then
     
 else # Linux
 
-    . ./$HELPERS_DIR/docker.sh
+    . ./.cicd/helpers/docker.sh
     
     # Generate Base Images
     # execute ./.cicd/generate-base-images.sh
