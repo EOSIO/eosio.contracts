a8836e083246f11096bf3647e533dc97525dee43
Scott Arnette <scott.a.arnette@gmail.com>
2019-08-09 10:50:43 -0400

    Enable remaining helpers.

--- .cicd/run.sh
+++ .cicd/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eo pipefail
-# . ./.cicd/helpers/general.sh
-# . ./.cicd/helpers/execute.sh
+. ./.cicd/helpers/general.sh
+. ./$HELPERS_DIR/execute.sh
 
 export FULL_TAG="eosio/producer:eos-binaries-trav-poc-contract-tests-1.8.0-e13ec7f756e78d9baf994c5d3a7bd643653d834b"
 export CDT_VERSION="1.6.2"
@@ -14,18 +14,18 @@ if [[ $(uname) == Darwin ]]; then
     cd build
     execute cmake ..
     execute make -j$JOBS
+    cd tests
+    execute ctest -j$JOBS -V --output-on-failure -T Test
     cd ..
     
 else # Linux
 
-    . ./.cicd/helpers/docker.sh
+    . ./$HELPERS_DIR/docker.sh
     
-    # Generate Base Images
-    # execute ./.cicd/generate-base-images.sh
     [[ -z $CDT_VERSION ]] && echo "Please specify CDT_VERSION." && exit 1
     CDT_COMMANDS="curl -LO https://github.com/EOSIO/eosio.cdt/releases/download/v$CDT_VERSION/eosio.cdt_$CDT_VERSION-1-ubuntu-18.04_amd64.deb && dpkg -i eosio.cdt_$CDT_VERSION-1-ubuntu-18.04_amd64.deb && export PATH=/usr/opt/eosio.cdt/$CDT_VERSION/bin:$PATH"
-    BUILD_COMMANDS="mkdir -p /workdir/build && cd /workdir/build && cmake -DCMAKE_CXX_COMPILER='clang++' -DCMAKE_C_COMPILER='clang' -DCMAKE_FRAMEWORK_PATH='/usr/local' .. && make -j 1"
-    TEST_COMMANDS="cd /workdir/build/tests && ctest -j 1 -V --output-on-failure -T Test"
+    BUILD_COMMANDS="mkdir -p /workdir/build && cd /workdir/build && cmake -DCMAKE_CXX_COMPILER='clang++' -DCMAKE_C_COMPILER='clang' -DCMAKE_FRAMEWORK_PATH='/usr/local' .. && make -j $JOBS"
+    TEST_COMMANDS="cd /workdir/build/tests && ctest -j $JOBS -V --output-on-failure -T Test"
 
     # Docker Run Arguments
     ARGS=${ARGS:-"--rm -v $(pwd):/workdir"}
