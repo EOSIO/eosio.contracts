2dcb23e4ee076b9481fc35a0f08344f058587ee2
arhag <arhag@users.noreply.github.com>
2019-09-01 23:02:22 -0400

    handle overflow check properly (avoid undefined behavior)

--- contracts/eosio.system/src/producer_pay.cpp
+++ contracts/eosio.system/src/producer_pay.cpp
@@ -82,8 +82,10 @@ namespace eosiosystem {
       const auto usecs_since_last_fill = (ct - _gstate.last_pervote_bucket_fill).count();
 
       if( usecs_since_last_fill > 0 && _gstate.last_pervote_bucket_fill > time_point() ) {
-         int64_t new_tokens = (_gstate4.continuous_rate * double(token_supply.amount) * double(usecs_since_last_fill)) / double(useconds_per_year);
-         check( new_tokens >= 0, "overflow in calculating new tokens to be issued; inflation rate is too high" );
+         double additional_inflation = (_gstate4.continuous_rate * double(token_supply.amount) * double(usecs_since_last_fill)) / double(useconds_per_year);
+         check( additional_inflation <= double(std::numeric_limits<int64_t>::max() - ((1ll << 10) - 1)),
+                "overflow in calculating new tokens to be issued; inflation rate is too high" );
+         int64_t new_tokens = static_cast<int64_t>(additional_inflation);
 
          int64_t to_producers     = (new_tokens * uint128_t(pay_factor_precision)) / _gstate4.inflation_pay_factor;
          int64_t to_savings       = new_tokens - to_producers;
