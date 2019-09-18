ff69b17e75193acd9c7b636256db3382c11cd406
Zach Butler <zach.butler@block.one>
2019-09-18 16:21:02 -0400

    Remove empty lines

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
 set -eo pipefail
-
 . ./.cicd/helpers/general.sh
 . ./.cicd/helpers/dependency-info.sh
-
 mkdir -p $BUILD_DIR
-
 FULL_TAG=${FULL_TAG:-eosio/ci-contracts-builder:base-ubuntu-18.04-$EOSIO_COMMIT}
 ARGS=${ARGS:-"--rm -v $(pwd):$MOUNTED_DIR"}
 CDT_COMMANDS="apt-get install -y wget && wget -q $CDT_URL -O eosio.cdt.deb && dpkg -i eosio.cdt.deb && export PATH=/usr/opt/eosio.cdt/$CDT_VERSION/bin:/usr/local/eosio/bin:\\\$PATH"
-
 PRE_COMMANDS="$CDT_COMMANDS && cd $MOUNTED_DIR/build"
 BUILD_COMMANDS="cmake .. && make -j $JOBS"
-
 COMMANDS="$PRE_COMMANDS && $BUILD_COMMANDS"
-
 # Load BUILDKITE Environment Variables for use in docker run
 if [[ -f $BUILDKITE_ENV_FILE ]]; then
     evars=""
@@ -22,5 +16,4 @@ if [[ -f $BUILDKITE_ENV_FILE ]]; then
         evars="$evars --env ${var%%=*}"
     done < "$BUILDKITE_ENV_FILE"
 fi
-
 eval docker run $ARGS $evars $FULL_TAG bash -c \"$COMMANDS\"
