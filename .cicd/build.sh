3442360d5b4854b6e4961b2b9dc1315b20b4bf7a
Zach Butler <zach.butler@block.one>
2019-09-19 10:36:47 -0400

    Always use the same container to build and test

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -4,6 +4,7 @@ set -eo pipefail
 . ./.cicd/helpers/dependency-info.sh
 mkdir -p $BUILD_DIR
 FULL_TAG=${FULL_TAG:-eosio/ci-contracts-builder:base-ubuntu-18.04-$EOSIO_COMMIT}
+buildkite-agent meta-data set docker-image "$FULL_TAG"
 ARGS=${ARGS:-"--rm -v $(pwd):$MOUNTED_DIR"}
 CDT_COMMANDS="apt-get install -y wget && wget -q $CDT_URL -O eosio.cdt.deb && dpkg -i eosio.cdt.deb && export PATH=/usr/opt/eosio.cdt/$CDT_VERSION/bin:\\\$PATH"
 PRE_COMMANDS="$CDT_COMMANDS && cd $MOUNTED_DIR/build"
