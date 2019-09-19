2ce7acfac6b23477589c515fd7d14518dc5f77a5
Zach Butler <zach.butler@block.one>
2019-09-19 15:29:23 -0400

    Export dependency and Docker information as variables on Travis

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -5,9 +5,15 @@ set -eo pipefail
 . ./.cicd/helpers/dependency-info.sh
 mkdir -p $BUILD_DIR
 DOCKER_IMAGE=${DOCKER_IMAGE:-eosio/ci-contracts-builder:base-ubuntu-18.04-$EOSIO_COMMIT}
-buildkite-agent meta-data set docker-image "$DOCKER_IMAGE"
-buildkite-agent meta-data set cdt-url "$CDT_URL"
-buildkite-agent meta-data set cdt-version "$CDT_VERSION"
+if [[ "$BUILDKITE" == 'true' ]]; then
+    buildkite-agent meta-data set cdt-url "$CDT_URL"
+    buildkite-agent meta-data set cdt-version "$CDT_VERSION"
+    buildkite-agent meta-data set docker-image "$DOCKER_IMAGE"
+else
+    export CDT_URL
+    export CDT_VERSION
+    export DOCKER_IMAGE
+fi
 ARGS=${ARGS:-"--rm -v $(pwd):$MOUNTED_DIR"}
 CDT_COMMANDS="apt-get install -y wget && wget -q $CDT_URL -O eosio.cdt.deb && dpkg -i eosio.cdt.deb && export PATH=/usr/opt/eosio.cdt/$CDT_VERSION/bin:\\\$PATH"
 PRE_COMMANDS="$CDT_COMMANDS && cd $MOUNTED_DIR/build"
