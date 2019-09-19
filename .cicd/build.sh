d8ed1ec6f23011997f6222d328aba4aed82537e9
Zach Butler <zach.butler@block.one>
2019-09-19 10:42:04 -0400

    Rename FULL_TAG to DOCKER_IMAGE for clarity

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -3,8 +3,8 @@ set -eo pipefail
 . ./.cicd/helpers/general.sh
 . ./.cicd/helpers/dependency-info.sh
 mkdir -p $BUILD_DIR
-FULL_TAG=${FULL_TAG:-eosio/ci-contracts-builder:base-ubuntu-18.04-$EOSIO_COMMIT}
-buildkite-agent meta-data set docker-image "$FULL_TAG"
+DOCKER_IMAGE=${DOCKER_IMAGE:-eosio/ci-contracts-builder:base-ubuntu-18.04-$EOSIO_COMMIT}
+buildkite-agent meta-data set docker-image "$DOCKER_IMAGE"
 ARGS=${ARGS:-"--rm -v $(pwd):$MOUNTED_DIR"}
 CDT_COMMANDS="apt-get install -y wget && wget -q $CDT_URL -O eosio.cdt.deb && dpkg -i eosio.cdt.deb && export PATH=/usr/opt/eosio.cdt/$CDT_VERSION/bin:\\\$PATH"
 PRE_COMMANDS="$CDT_COMMANDS && cd $MOUNTED_DIR/build"
@@ -19,9 +19,9 @@ if [[ -f $BUILDKITE_ENV_FILE ]]; then
 fi
 # retry docker pull to protect against failures due to race conditions with eosio pipeline
 INDEX='1'
-echo "$ docker pull $FULL_TAG"
-while [[ "$(docker pull $FULL_TAG 2>&1 | grep -ice "manifest for $FULL_TAG not found")" != '0' ]]; do
-    echo "ERROR: Docker image \"$FULL_TAG\" not found for eosio commit ${EOSIO_COMMIT:0:7} from \"$EOSIO_VERSION\""'!'
+echo "$ docker pull $DOCKER_IMAGE"
+while [[ "$(docker pull $DOCKER_IMAGE 2>&1 | grep -ice "manifest for $DOCKER_IMAGE not found")" != '0' ]]; do
+    echo "ERROR: Docker image \"$DOCKER_IMAGE\" not found for eosio commit ${EOSIO_COMMIT:0:7} from \"$EOSIO_VERSION\""'!'
     printf "There must be a successful build against ${EOSIO_COMMIT:0:7} \033]1339;url=https://eos-coverage.s3-us-west-2.amazonaws.com/build-$BUILDKITE_BUILD_NUMBER/code-coverage-report/index.html;content=here\a for this container to exist.\n"
     echo "Attempt $INDEX, retry in 60 seconds..."
     echo ''
@@ -29,4 +29,4 @@ while [[ "$(docker pull $FULL_TAG 2>&1 | grep -ice "manifest for $FULL_TAG not f
     sleep 60
 done
 # run
-eval docker run $ARGS $evars $FULL_TAG bash -c \"$COMMANDS\"
+eval docker run $ARGS $evars $DOCKER_IMAGE bash -c \"$COMMANDS\"
