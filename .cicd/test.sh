4c38cf50d2e852f68f6633b7a558f74e35af894e
Zach Butler <zach.butler@block.one>
2019-09-19 11:36:03 -0400

    Remove dependency-info.sh from test.sh

--- .cicd/test.sh
+++ .cicd/test.sh
@@ -2,7 +2,6 @@
 set -eo pipefail
 . ./.cicd/helpers/buildkite.sh
 . ./.cicd/helpers/general.sh
-. ./.cicd/helpers/dependency-info.sh
 mkdir -p $BUILD_DIR
 DOCKER_IMAGE="$(buildkite-agent meta-data get docker-image)"
 ARGS=${ARGS:-"--rm -v $(pwd):$MOUNTED_DIR"}
