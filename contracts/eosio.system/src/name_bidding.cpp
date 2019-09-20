761457f5bf88d6a1bd4875c53c299efb6b348c8f
Andrianto Lie <andrianto.lie@block.one>
2019-09-20 14:15:56 +0800

    disable name bidding

--- contracts/eosio.system/src/name_bidding.cpp
+++ contracts/eosio.system/src/name_bidding.cpp
@@ -9,6 +9,7 @@ namespace eosiosystem {
    using eosio::token;
 
    void system_contract::bidname( const name& bidder, const name& newname, const asset& bid ) {
+      check( false, "bidname is disabled in the testnet" );
       require_auth( bidder );
       check( newname.suffix() == newname, "you can only bid on top-level suffix" );
 
@@ -68,6 +69,7 @@ namespace eosiosystem {
    }
 
    void system_contract::bidrefund( const name& bidder, const name& newname ) {
+      check( false, "bidrefund is disabled in the testnet" );
       bid_refund_table refunds_table(get_self(), newname.value);
       auto it = refunds_table.find( bidder.value );
       check( it != refunds_table.end(), "refund not found" );
