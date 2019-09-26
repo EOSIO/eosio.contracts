b9aadd2a7a0958fce75236c9bdd7476f648a01d0
Sandwich <dskvr@users.noreply.github.com>
2019-09-26 15:18:42 +0200

    update bios contract annotations, simplification, fix newaccount docs

--- contracts/eosio.bios/include/eosio.bios/eosio.bios.hpp
+++ contracts/eosio.bios/include/eosio.bios/eosio.bios.hpp
@@ -44,14 +44,11 @@ namespace eosio {
    using eosio::permission_level;
    using eosio::public_key;
 
-   /**
-    * A weighted permission.
-    *
-    * @details Defines a weighted permission, that is a permission which has a weight associated.
-    * A permission is defined by an account name plus a permission name. The weight is going to be
-    * used against a threshold, if the weight is equal or greater than the threshold set then authorization
-    * will pass.
-    */
+   // A weighted permission.
+   // @details Defines a weighted permission, that is a permission which has a weight associated.
+   // A permission is defined by an account name plus a permission name. The weight is going to be
+   // used against a threshold, if the weight is equal or greater than the threshold set then authorization
+   // will pass.
    struct permission_level_weight {
       permission_level  permission;
       uint16_t          weight;
@@ -60,11 +57,8 @@ namespace eosio {
       EOSLIB_SERIALIZE( permission_level_weight, (permission)(weight) )
    };
 
-   /**
-    * Weighted key.
-    *
-    * @details A weighted key is defined by a public key and an associated weight.
-    */
+   // Weighted key.
+   // @details A weighted key is defined by a public key and an associated weight.
    struct key_weight {
       eosio::public_key  key;
       uint16_t           weight;
@@ -73,11 +67,8 @@ namespace eosio {
       EOSLIB_SERIALIZE( key_weight, (key)(weight) )
    };
 
-   /**
-    * Wait weight.
-    *
-    * @details A wait weight is defined by a number of seconds to wait for and a weight.
-    */
+   // Wait weight.
+   // @details A wait weight is defined by a number of seconds to wait for and a weight.
    struct wait_weight {
       uint32_t           wait_sec;
       uint16_t           weight;
@@ -86,15 +77,11 @@ namespace eosio {
       EOSLIB_SERIALIZE( wait_weight, (wait_sec)(weight) )
    };
 
-   /**
-    * Blockchain authority.
-    *
-    * @details An authority is defined by:
-    * - a vector of key_weights (a key_weight is a public key plus a weight),
-    * - a vector of permission_level_weights, (a permission_level is an account name plus a permission name)
-    * - a vector of wait_weights (a wait_weight is defined by a number of seconds to wait and a weight)
-    * - a threshold value
-    */
+   // Blockchain authority.
+   // - a vector of key_weights (a key_weight is a public key plus a weight),
+   // - a vector of permission_level_weights, (a permission_level is an account name plus a permission name)
+   // - a vector of wait_weights (a wait_weight is defined by a number of seconds to wait and a weight)
+   // - a threshold value
    struct authority {
       uint32_t                              threshold = 0;
       std::vector<key_weight>               keys;
@@ -105,19 +92,16 @@ namespace eosio {
       EOSLIB_SERIALIZE( authority, (threshold)(keys)(accounts)(waits) )
    };
 
-   /**
-    * Blockchain block header.
-    *
-    * @details A block header is defined by:
-    * - a timestamp,
-    * - the producer that created it,
-    * - a confirmed flag default as zero,
-    * - a link to previous block,
-    * - a link to the transaction merkel root,
-    * - a link to action root,
-    * - a schedule version,
-    * - and a producers' schedule.
-    */
+   // Blockchain block header.
+   // @details A block header is defined by:
+   // - a timestamp,
+   // - the producer that created it,
+   // - a confirmed flag default as zero,
+   // - a link to previous block,
+   // - a link to the transaction merkel root,
+   // - a link to action root,
+   // - a schedule version,
+   // - and a producers' schedule.
    struct block_header {
       uint32_t                                  timestamp;
       name                                      producer;
@@ -134,38 +118,28 @@ namespace eosio {
    };
 
    /**
-    * @defgroup eosiobios eosio.bios
-    * @ingroup eosiocontracts
-    *
     * eosio.bios is a minimalistic system contract that only supplies the actions that are absolutely
     * critical to bootstrap a chain and nothing more.
-    *
-    * @{
     */
    class [[eosio::contract("eosio.bios")]] bios : public contract {
       public:
          using contract::contract;
-         /**
-          * @{
-          * These actions map one-on-one with the ones defined in
-          * [Native Action Handlers](@ref native_action_handlers) section.
-          * They are present here so they can show up in the abi file and thus user can send them
-          * to this contract, but they have no specific implementation at this contract level,
-          * they will execute the implementation at the core level and nothing else.
-          */
+         // These actions map one-on-one with the ones defined in
+         // [Native Action Handlers](@ref native_action_handlers) section.
+         // They are present here so they can show up in the abi file and thus user can send them
+         // to this contract, but they have no specific implementation at this contract level,
+         // they will execute the implementation at the core level and nothing else.
+
          /**
           * New account action
           *
-          * @details Called after a new account is created. This code enforces resource-limits rules
+          * @details Used to create new accounts. This code enforces resource-limits rules
           * for new accounts as well as new account naming conventions.
           *
-          * 1. accounts cannot contain '.' symbols which forces all acccounts to be 12
-          * characters long without '.' until a future account auction process is implemented
-          * which prevents name squatting.
-          *
-          * 2. new accounts must stake a minimal number of tokens (as set in system parameters)
-          * therefore, this method will execute an inline buyram from receiver for newacnt in
-          * an amount equal to the current new account creation fee.
+          * @param creator - The accountname of the creator. An account must be created from an existing account.
+          * @param name - The name of the desired account
+          * @param owner - The public key to associate to the new account's "owner" authority
+          * @param active - The public key to associate to the new account's "activee" authority
           */
          [[eosio::action]]
          void newaccount( name             creator,
@@ -366,11 +340,8 @@ namespace eosio {
          [[eosio::action]]
          void reqactivated( const eosio::checksum256& feature_digest );
 
-         /**
-          * Abi hash structure
-          *
-          * @details Abi hash structure is defined by contract owner and the contract hash.
-          */
+         // Abi hash structure
+         // @details Abi hash structure is defined by contract owner and the contract hash.
          struct [[eosio::table]] abi_hash {
             name              owner;
             checksum256       hash;
@@ -379,9 +350,7 @@ namespace eosio {
             EOSLIB_SERIALIZE( abi_hash, (owner)(hash) )
          };
 
-         /**
-          * Multi index table that stores the contracts' abi index by their owners/accounts.
-          */
+         // Multi index table that stores the contracts' abi index by their owners/accounts.
          typedef eosio::multi_index< "abihash"_n, abi_hash > abi_hash_table;
 
          using newaccount_action = action_wrapper<"newaccount"_n, &bios::newaccount>;
@@ -400,5 +369,4 @@ namespace eosio {
          using activate_action = action_wrapper<"activate"_n, &bios::activate>;
          using reqactivated_action = action_wrapper<"reqactivated"_n, &bios::reqactivated>;
    };
-   /** @}*/ // end of @defgroup eosiobios eosio.bios
-} /// namespace eosio
+}
