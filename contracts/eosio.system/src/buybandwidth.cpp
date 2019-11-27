71830da94e90130dccb1a8258844488358ad4946
Todd Fleming <todd.fleming@block.one>
2019-11-27 16:00:30 -0500

    buybandwidth

---
+++ contracts/eosio.system/src/buybandwidth.cpp
@@ -0,0 +1,14 @@
+#include <eosio.system/eosio.system.hpp>
+
+namespace eosiosystem {
+
+void system_contract::configbuybw(const buybw_config& args) {
+   //
+}
+
+void system_contract::buybandwidth(const name& payer, const name& receiver, uint32_t days, int64_t net, int64_t cpu,
+                                   const asset& max_payment) {
+   //
+}
+
+} // namespace eosiosystem
