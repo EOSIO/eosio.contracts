9a5df11867019a75933efac63d10e55aac0ec2b5
Todd Fleming <todd.fleming@block.one>
2019-11-27 11:24:28 -0500

    resource31

---
+++ tests/eosio.resource31_tests.cpp
@@ -0,0 +1,24 @@
+#include <Runtime/Runtime.h>
+#include <boost/test/unit_test.hpp>
+#include <cstdlib>
+#include <eosio/chain/contract_table_objects.hpp>
+#include <eosio/chain/exceptions.hpp>
+#include <eosio/chain/global_property_object.hpp>
+#include <eosio/chain/resource_limits.hpp>
+#include <eosio/chain/wast_to_wasm.hpp>
+#include <fc/log/logger.hpp>
+#include <iostream>
+#include <sstream>
+
+#include "eosio.system_tester.hpp"
+
+using namespace eosio_system;
+
+BOOST_AUTO_TEST_SUITE(eosio_system_resource31_tests)
+
+BOOST_FIXTURE_TEST_CASE(foo, eosio_system_tester) try {
+  //
+}
+FC_LOG_AND_RETHROW()
+
+BOOST_AUTO_TEST_SUITE_END()
