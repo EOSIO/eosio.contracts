0641c09a8f30a0eb0b9f3df95c4098e7c9f3c43c
Todd Fleming <todd.fleming@block.one>
2019-12-02 11:54:25 -0500

    rentbw

--- contracts/eosio.system/src/rentbw.cpp
+++ contracts/eosio.system/src/rentbw.cpp
@@ -54,8 +54,8 @@ void system_contract::adjust_resources(name payer, name account, symbol core_sym
    }
 } // system_contract::adjust_resources
 
-void system_contract::process_buybw_queue(symbol core_symbol, buybw_state& state, buybw_order_table& orders,
-                                          uint32_t max_items) {
+void system_contract::process_rentbw_queue(symbol core_symbol, rentbw_state& state, rentbw_order_table& orders,
+                                           uint32_t max_items) {
    time_point_sec now       = eosio::current_time_point();
    auto           idx       = orders.get_index<"byexpires"_n>();
    int64_t        total_net = 0;
@@ -74,7 +74,7 @@ void system_contract::process_buybw_queue(symbol core_symbol, buybw_state& state
    adjust_resources(get_self(), reserv_account, core_symbol, total_net, total_cpu, true);
 }
 
-void update_weight(time_point_sec now, buybw_state_resource& res) {
+void update_weight(time_point_sec now, rentbw_state_resource& res) {
    if (now >= res.target_timestamp)
       res.weight = res.target_weight;
    else
@@ -84,7 +84,7 @@ void update_weight(time_point_sec now, buybw_state_resource& res) {
                          (res.target_timestamp.utc_seconds - res.initial_timestamp.utc_seconds);
 }
 
-void update_utilization(time_point_sec now, buybw_state_resource& res) {
+void update_utilization(time_point_sec now, rentbw_state_resource& res) {
    if (res.utilization >= res.adjusted_utilization)
       res.adjusted_utilization = res.utilization;
    else
@@ -95,7 +95,7 @@ void update_utilization(time_point_sec now, buybw_state_resource& res) {
    res.utilization_timestamp = now;
 }
 
-void system_contract::update_buybw_state(buybw_state& state) {
+void system_contract::update_rentbw_state(rentbw_state& state) {
    time_point_sec now = eosio::current_time_point();
    update_weight(now, state.net);
    update_weight(now, state.cpu);
@@ -103,12 +103,12 @@ void system_contract::update_buybw_state(buybw_state& state) {
    update_utilization(now, state.cpu);
 }
 
-void system_contract::configbuybw(buybw_config& args) {
+void system_contract::configrentbw(rentbw_config& args) {
    require_auth(get_self());
-   time_point_sec        now         = eosio::current_time_point();
-   auto                  core_symbol = get_core_symbol();
-   buybw_state_singleton state_sing{ get_self(), 0 };
-   auto                  state = state_sing.get_or_default();
+   time_point_sec         now         = eosio::current_time_point();
+   auto                   core_symbol = get_core_symbol();
+   rentbw_state_singleton state_sing{ get_self(), 0 };
+   auto                   state = state_sing.get_or_default();
 
    auto update = [&](auto& state, auto& args, auto& delta_weight) {
       if (args.current_weight == args.target_weight)
@@ -121,23 +121,23 @@ void system_contract::configbuybw(buybw_config& args) {
       eosio::check(args.decay_secs >= 1, "decay_secs must be >= 1");
       eosio::check(args.total_price.symbol == core_symbol, "total_price doesn't match core symbol");
       eosio::check(args.total_price.amount > 0, "total_price must be positive");
-      eosio::check(args.min_purchase_price.symbol == core_symbol, "min_purchase_price doesn't match core symbol");
+      eosio::check(args.min_rent_price.symbol == core_symbol, "min_rent_price doesn't match core symbol");
 
       delta_weight = args.current_weight - state.weight;
 
-      state.weight             = args.current_weight;
-      state.initial_weight     = args.current_weight;
-      state.target_weight      = args.target_weight;
-      state.initial_timestamp  = now;
-      state.target_timestamp   = args.target_timestamp;
-      state.exponent           = args.exponent;
-      state.decay_secs         = args.decay_secs;
-      state.total_price        = args.total_price;
-      state.min_purchase_price = args.min_purchase_price;
+      state.weight            = args.current_weight;
+      state.initial_weight    = args.current_weight;
+      state.target_weight     = args.target_weight;
+      state.initial_timestamp = now;
+      state.target_timestamp  = args.target_timestamp;
+      state.exponent          = args.exponent;
+      state.decay_secs        = args.decay_secs;
+      state.total_price       = args.total_price;
+      state.min_rent_price    = args.min_rent_price;
    };
 
-   eosio::check(args.purchase_days > 0, "purchase_days must be > 0");
-   state.purchase_days = args.purchase_days;
+   eosio::check(args.rent_days > 0, "rent_days must be > 0");
+   state.rent_days = args.rent_days;
 
    int64_t net_delta = 0;
    int64_t cpu_delta = 0;
@@ -146,18 +146,18 @@ void system_contract::configbuybw(buybw_config& args) {
 
    adjust_resources(get_self(), reserv_account, core_symbol, net_delta, cpu_delta, true);
    state_sing.set(state, get_self());
-} // system_contract::configbuybw
+} // system_contract::configrentbw
 
-void system_contract::buybandwidth(const name& payer, const name& receiver, uint32_t days, int64_t net, int64_t cpu,
-                                   const asset& max_payment) {
+void system_contract::rentbw(const name& payer, const name& receiver, uint32_t days, int64_t net, int64_t cpu,
+                             const asset& max_payment) {
    require_auth(payer);
-   buybw_state_singleton state_sing{ get_self(), 0 };
-   buybw_order_table     orders{ get_self(), 0 };
-   eosio::check(state_sing.exists(), "buybandwidth hasn't been initialized");
+   rentbw_state_singleton state_sing{ get_self(), 0 };
+   rentbw_order_table     orders{ get_self(), 0 };
+   eosio::check(state_sing.exists(), "rentbw hasn't been initialized");
    auto state       = state_sing.get();
    auto core_symbol = get_core_symbol();
-   process_buybw_queue(core_symbol, state, orders, 2);
-   update_buybw_state(state);
+   process_rentbw_queue(core_symbol, state, orders, 2);
+   update_rentbw_state(state);
    state_sing.set(state, get_self());
 }
 
