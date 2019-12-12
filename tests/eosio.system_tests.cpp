3647ed9e93811a7663d605acae3bdfe1b8267500
Khaled Al-Hassanieh <khaled.alhassanieh@gmail.com>
2019-12-11 19:47:03 -0500

    REX changes - tests

--- tests/eosio.system_tests.cpp
+++ tests/eosio.system_tests.cpp
@@ -4913,7 +4913,10 @@ BOOST_FIXTURE_TEST_CASE( update_rex, eosio_system_tester, * boost::unit_test::to
    produce_blocks( 1 );
    BOOST_REQUIRE_EQUAL( success(), sellrex( alice, get_rex_balance( alice ) ) );
    BOOST_REQUIRE_EQUAL( 0,         get_rex_balance( alice ).get_amount() );
-   BOOST_REQUIRE_EQUAL( success(), vote( alice, std::vector<account_name>(producer_names[0], producer_names[4]) ) );
+   std::vector<account_name> two_producers;
+   two_producers.push_back( producer_names[0] );
+   two_producers.push_back( producer_names[4] );
+   BOOST_REQUIRE_EQUAL( success(), vote( alice, two_producers ) );
    BOOST_REQUIRE_EQUAL( wasm_assert_msg("must vote for at least 21 producers or for a proxy before buying REX"),
                         buyrex( alice, core_sym::from_string("1.0000") ) );
 
