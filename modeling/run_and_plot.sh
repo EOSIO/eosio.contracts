73aae4a5eea434249ad188cae705f336d3955ab1
Maxim Kirov <maxim.kirov@block.one>
2020-09-30 18:32:46 -0400

    added check for gnuplot

--- modeling/run_and_plot.sh
+++ modeling/run_and_plot.sh
@@ -9,23 +9,28 @@ fi
 ../build/tests/unit_test -t eosio_system_rentbw_modeling_tests -- $1 $2 $3
 
 if [ $? -eq 0 ]; then
-    echo "
-      set datafile separator ';'
-      set style line 1 lt 1 lc rgb '#A00000' lw 2 pt 7 ps 1
-      set style line 2 lt 1 lc rgb '#00A000' lw 2 pt 11 ps 1
+	command -v gnuplot 1>/dev/null 2>&1
+	if [ $? -eq 0 ]; then
+        echo "
+          set datafile separator ';'
+          set style line 1 lt 1 lc rgb '#A00000' lw 2 pt 7 ps 1
+          set style line 2 lt 1 lc rgb '#00A000' lw 2 pt 11 ps 1
   
-      set style line 81 lt 0 lc rgb '#808080' lw 0.5
+          set style line 81 lt 0 lc rgb '#808080' lw 0.5
 
-      # Draw the grid lines for both the major and minor tics
-      set grid xtics
-      set grid ytics
-      set grid mxtics
-      set grid mytics
+          # Draw the grid lines for both the major and minor tics
+          set grid xtics
+          set grid ytics
+          set grid mxtics
+          set grid mytics
 
-      # Put the grid behind anything drawn and use the linestyle 81
-      set grid back ls 81
+          # Put the grid behind anything drawn and use the linestyle 81
+          set grid back ls 81
 
-      set key autotitle columnhead
-      plot \"$3\" using 1:24 w lp ls 1, '' using 1:25 w lp ls 2
-	" | gnuplot --persist
+          set key autotitle columnhead
+          plot \"$3\" using 1:24 w lp ls 1, '' using 1:25 w lp ls 2
+	    " | gnuplot --persist
+    else
+        echo "INFO: You can install gnuplot to see the result graph"
+    fi
 fi
